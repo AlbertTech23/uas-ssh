acf83a116f8a6557a57791b008e8adcbcacc64df
hawyeet <kanzaamanda283@gmail.com>
2023-11-24 13:07:00 +0700

    about us v.1.0
    Created component. Can't get the top and bottom margins for the brands div to work somehow.

--- src/components/AboutComponent/AboutComponent.jsx
+++ src/components/AboutComponent/AboutComponent.jsx
@@ -1,7 +1,43 @@
-import "./AboutComponent.scss";
+import React from 'react';
+import './AboutComponent.scss';
+import thumbnail from './assets/about-us.png';
+import strip from './assets/about-us-strip.png';
 
 const AboutComponent = () => {
-  return <div className="p-2">About Us</div>;
+  return (
+    <div>
+      <div className="container mx-auto mt-12 mb-12 px-20 grid grid-cols-2 gap-4">
+      <div className="flex items-center justify-center">
+        <img src={thumbnail} className="h-50" alt="About Us Thumbnail" />
+      </div>
+      <div className="flex-container mt-12">
+        <div className="flex items-center">
+          <img src={strip} className="h-20" alt="About Us Strip" />
+          <h1 id="heading" className="strip-height m-4">
+            ABOUT US
+          </h1>
+        </div>
+        <p id="desc">
+          Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa
+          mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla,
+          mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis
+          tellus. Nullam quis imperdiet augue. Vestibulum auctor ornare leo,
+          non suscipit magna interdum eu. Curabitur pellentesque nibh nibh, at
+          maximus ante fermentum sit amet. Pellentesque commodo lacus at sodales
+          sodales. Quisque sagittis orci ut diam condimentum, vel euismod erat
+          placerat. In iaculis arcu eros, eget tempus orci facilisis id.
+        </p>
+        <a href="#"><button id="button" className="mt-5">READ MORE</button></a>
+      </div>
+    </div>
+      <div id="brands" className="container mt-5 mb-5">
+        <h1>BRAND</h1>
+        <h1>BRAND</h1>
+        <h1>BRAND</h1>
+        <h1>BRAND</h1>
+      </div>
+    </div>
+  );
 };
 
 export default AboutComponent;
