6cb1e141d67c4387338b5824d6fb30ee0aed5a3e
hawyeet <kanzaamanda283@gmail.com>
2023-12-05 19:05:50 +0700

    created our menu page

--- src/components/OurMenu/OurMenu.jsx
+++ src/components/OurMenu/OurMenu.jsx
@@ -1,14 +1,50 @@
 import "./OurMenu.scss";
+import MenuCard from "./MenuCard";
+import ourmenu from "./assets/ourmenu.png";
+import menu3 from "./assets/menu3.png";
 // import "./fonts/Forum-Regular.ttf";
 
 const OurMenu = () => {
-    return (
-        <div className="container">
-        titit
+  const MenuInfo = [
+    {
+      id: 1,
+      name: "Appetizers",
+      image: menu3,
+    },
+    {
+      id: 2,
+      name: "Desserts",
+      image: menu3,
+    },
+    {
+      id: 3,
+      name: "Main Dishes",
+      image: menu3,
+    },
+    {
+      id: 4,
+      name: "Drinks",
+      image: menu3,
+    },
+  ];
+
+  return (
+    <div className="flex-1 items-center container mx-auto mt-12">
+      <div className="flex flex-col items-center justify-center mt-7 mb-7">
+        <h1 id="heading">
+          - OUR MENU -
+        </h1>
+        <img src={ ourmenu }></img>
       </div>
-    )
-    ;
-  };
-  
+      <div className="mx-2 mt-16 md:ml-20 md:mr-20 grid grid-cols-2 grid-rows-2 gap-4 place-items-center">
+        {MenuInfo.map((menuItem) => (
+          <div key={menuItem.id}>
+            <MenuCard name={menuItem.name} image={menuItem.image} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
 export default OurMenu;
-  
