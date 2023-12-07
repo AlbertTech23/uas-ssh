49f849c77ac958eceecc6ca91bd5ae27c995ad7b
AlbertTech23 <alberttirtokusumo@gmail.com>
2023-12-07 22:11:50 +0700

    adding animation to coming soon page

--- src/pages/ComingSoon/ComingSoon.jsx
+++ src/pages/ComingSoon/ComingSoon.jsx
@@ -1,18 +1,54 @@
 import "./ComingSoon.scss";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 const ComingSoon = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 2000,
+      once: true,
+    });
+  }, []);
+
   return (
     <div className="comingsoon-container justify-center">
-      <div className="middle-content flex flex-col items-center ">
+      <div
+        className="middle-content flex flex-col items-center"
+        // data-aos="fade-up"
+      >
         <img
           className="logo-ssh-comingsoon mb-10"
           src="src/assets/gambar/SSH_-_LOGO 1.png"
           alt="logo-ssh"
+          data-aos="fade-up"
         />
-        <span className="above-footer">WE'RE STILL</span>
-        <span className="above-footer-title mb-5">Cooking Our Website</span>
-        <img src="src/assets/comingsoonbatik.svg" alt="batik-pattern" />
-        <span className="above-footer-subtitle mt-8 mb-16">
+        <span
+          className="above-footer"
+          data-aos="fade-up"
+          data-aos-duration="2000"
+        >
+          WE'RE STILL
+        </span>
+        <span
+          className="above-footer-title mb-5"
+          data-aos="fade-up"
+          data-aos-duration="3000"
+        >
+          Cooking Our Website
+        </span>
+        <img
+          data-aos="fade-up"
+          data-aos-delay="400"
+          src="src/assets/comingsoonbatik.svg"
+          alt="batik-pattern"
+        />
+        <span
+          className="above-footer-subtitle mt-8 mb-16"
+          data-aos="fade-up"
+          data-aos-delay="1300"
+          data-aos-duration="4000"
+        >
           We are going to launch our website very soon!
         </span>
       </div>
