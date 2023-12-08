1e7e02612d224352d4551f3c7fe66efd59a7f5c9
AlbertTech23 <alberttirtokusumo@gmail.com>
2023-12-08 22:00:55 +0700

    fixing classnames of ourmenu

--- src/components/OurMenu/MenuCard/MenuCard.jsx
+++ src/components/OurMenu/MenuCard/MenuCard.jsx
@@ -1,20 +1,23 @@
-import React from 'react';
+import React from "react";
 import "./MenuCard.scss";
 
 function MenuCard(props) {
-    return (    
-      <div className="flex-container">
-        <div className="card card-bordered rounded-lg xl:my-8 my-2 mx-2 mrelative">
-          <div className="flex items-end justify-center card-image relative overflow-hidden rounded-lg">
-            <img src={props.image} className="menu-image object-cover object-center" alt={props.name} />
-            <button className="button absolute flex items-center justify-center md:mb-4 mb-2">
-              {props.name}
-            </button>
-          </div>
+  return (
+    <div className="flex-container">
+      <div className="card card-bordered rounded-lg xl:my-8 my-2 mx-2 mrelative">
+        <div className="flex items-end justify-center card-image relative overflow-hidden rounded-lg">
+          <img
+            src={props.image}
+            className="menu-image object-cover object-center"
+            alt={props.name}
+          />
+          <button className="buttonOurMenu absolute flex items-center justify-center md:mb-4 mb-2">
+            {props.name}
+          </button>
         </div>
       </div>
-    );
-  };
-  
+    </div>
+  );
+}
+
 export default MenuCard;
-  
