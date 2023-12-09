0c039dd9beec7148aedd577532a9867218000b11
AlbertTech23 <alberttirtokusumo@gmail.com>
2023-12-09 11:22:13 +0700

    responsive not found done footer

--- src/pages/PageNotFound/PageNotFound.jsx
+++ src/pages/PageNotFound/PageNotFound.jsx
@@ -6,15 +6,26 @@ const PageNotFound = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="NotFoundContainer flex flex-col justify-center items-center">
+    <div className="NotFoundContainer flex flex-col justify-center items-center pb-10">
       <img className="w-[35%]" src={NotFound} alt="404NotFound" />
       <span className="notfoundtitle">PAGE NOT FOUND</span>
       <span className="notfoundsubtitle">
         The page you're looking for doesn't exist or has been moved
       </span>
-      <button className="buttonNotFound mt-10" onClick={navigate("/")}>
+      <button
+        className="buttonNotFound md:mt-10 mt-5"
+        onClick={() => {
+          navigate("/");
+        }}
+      >
         BACK TO HOME
       </button>
+      <div className="footer-comingsoon">
+        <span className="footer-word">
+          Copyright © <span className="footer-bold">Sumatra Sizzle House</span>{" "}
+          | All Rights Reserved
+        </span>
+      </div>
     </div>
   );
 };
