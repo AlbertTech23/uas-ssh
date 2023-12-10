e640828d5767d37d2366aa5c13557a33d33dbbab
hawyeet <kanzaamanda283@gmail.com>
2023-12-10 08:18:05 +0700

    created headermaster

---
+++ src/components/HeaderMaster/HeaderMaster.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './HeaderMaster.scss';
+
+const HeaderMaster = ({ imgSrc, subheading, heading, paragraph }) => {
+  return (
+    <div className="relative w-full">
+      <img src={imgSrc} className="w-full filter brightness-[30%]" alt="Header Image" />
+      <div className="text-center absolute transform -translate-y-1/2 left-5 right-5 top-1/2">
+        <h1 id="masterSubheading">{subheading}</h1>
+        <h1 id="masterHeading">{heading}</h1>
+        <p id="masterP">{paragraph}</p>
+      </div>
+    </div>
+  );
+};
+
+export default HeaderMaster;
