3d6d29d0c080427902e68dbacad9f31eb15e69a0
AlbertTech23 <alberttirtokusumo@gmail.com>
2023-12-20 12:59:15 +0700

    page not found reworked, without api still

--- src/pages/PageNotFound/PageNotFound.jsx
+++ src/pages/PageNotFound/PageNotFound.jsx
@@ -1,25 +1,54 @@
 import "./404PageNotFound.scss";
 import NotFound from "../../assets/404NotFound.svg";
+import ArrowCircleRight from "../../assets/ArrowCircleRight.svg";
 import { useNavigate } from "react-router-dom";
 
 const PageNotFound = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="NotFoundContainer flex flex-col justify-center items-center pb-10">
-      <img className="w-[35%]" src={NotFound} alt="404NotFound" />
-      <span className="notfoundtitle">PAGE NOT FOUND</span>
-      <span className="notfoundsubtitle">
-        The page you're looking for doesn't exist or has been moved
-      </span>
-      <button
-        className="buttonNotFound md:mt-10 mt-5"
-        onClick={() => {
-          navigate("/");
-        }}
-      >
-        BACK TO HOME
-      </button>
+    <div className="NotFoundContainer xl:flex lg:flex-row md:flex-col lg:items-center lg:justify-between flex-col">
+      <div className="leftNotFound xl:ps-40 lg:ps-16 md:ps-16 md:pt-14 pt-5 ps-8">
+        <div className="flex flex-col pb-10">
+          <img className="imageNotFound" src={NotFound} alt="404NotFound" />
+          <span className="notfoundtitle">PAGE NOT FOUND</span>
+          <span className="notfoundsubtitle">
+            The page you're looking for doesn't exist or has been moved
+          </span>
+          <div className="buttonNotFoundContainer pt-8">
+            BACK TO HOME
+            <button
+              className="arrowRightButton"
+              onClick={() => {
+                navigate("/");
+              }}
+            >
+              <img src={ArrowCircleRight} alt="arrowRight" />
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="rightNotFound xl:pe-40 lg:pe-16 md:pe-16 md:pt-10 pe-8">
+        <div className="notFoundQuoteContainer flex flex-col items-end">
+          <div className="notFoundQuotes flex flex-col items-end">
+            <span className="notFoundQuoteWord">
+              “Food is symbolic of love when words are inadequate.”
+            </span>
+            <span className="notFoundQuoteAuthor">— Alan D. Wolfelt</span>
+          </div>
+          <span className="wordNotFound pt-10">
+            Click the button below to generate a new quote
+          </span>
+        </div>
+        <div className="quotesButtonNFContainer pt-8">
+          <button className="quotesButtonNF">FAMILY</button>
+          <button className="quotesButtonNF">FOOD</button>
+          <button className="quotesButtonNF">HAPPINESS</button>
+          <button className="quotesButtonNF">DESIGN</button>
+          <button className="quotesButtonNF">EXPERIENCE</button>
+          <button className="quotesButtonNF">HEALTH</button>
+        </div>
+      </div>
       <div className="footer-comingsoon">
         <span className="footer-word">
           Copyright © <span className="footer-bold">Sumatra Sizzle House</span>{" "}
