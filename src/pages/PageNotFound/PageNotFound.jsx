44e681dfa22c3a8ea177babd842d3493222cfe8a
AlbertTech23 <alberttirtokusumo@gmail.com>
2023-12-20 13:17:10 +0700

    API BERES NOT FOUND BERES

--- src/pages/PageNotFound/PageNotFound.jsx
+++ src/pages/PageNotFound/PageNotFound.jsx
@@ -1,10 +1,67 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./404PageNotFound.scss";
 import NotFound from "../../assets/404NotFound.svg";
 import ArrowCircleRight from "../../assets/ArrowCircleRight.svg";
 import { useNavigate } from "react-router-dom";
 
 const PageNotFound = () => {
+  const [quotesData, setQuotesData] = useState([]);
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [category, setCategory] = useState("food"); // Default category
   const navigate = useNavigate();
+
+  const fetchQuotes = async () => {
+    try {
+      setLoading(true);
+
+      const apiKey = "bFyBNgdmQ5Jx7Ria3PmGGw==kVMJtuSHr46XDi2H";
+
+      let response;
+      let quoteTooLong = true;
+
+      while (quoteTooLong) {
+        response = await axios.get(
+          `https://api.api-ninjas.com/v1/quotes?category=${category}`,
+          {
+            headers: {
+              "X-Api-Key": apiKey,
+            },
+          }
+        );
+
+        if (response.data && response.data.length > 0) {
+          const newQuote = response.data[0].quote;
+          const wordCount = newQuote.split(" ").length;
+
+          if (wordCount <= 30) {
+            setQuotesData(response.data);
+            setQuote(newQuote);
+            setAuthor(response.data[0].author);
+            quoteTooLong = false;
+          }
+        } else {
+          console.error("Error fetching quotes: Empty or undefined data");
+          break;
+        }
+      }
+    } catch (error) {
+      console.error("Error fetching quotes:", error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchQuotes();
+  }, [category]);
+
+  const handleCategoryChange = (newCategory) => {
+    setCategory(newCategory);
+    fetchQuotes();
+  };
 
   return (
     <div className="NotFoundContainer xl:flex lg:flex-row md:flex-col lg:items-center lg:justify-between flex-col">
@@ -31,22 +88,56 @@ const PageNotFound = () => {
       <div className="rightNotFound xl:pe-40 lg:pe-16 md:pe-16 md:pt-10 pe-8">
         <div className="notFoundQuoteContainer flex flex-col items-end">
           <div className="notFoundQuotes flex flex-col items-end">
-            <span className="notFoundQuoteWord">
-              “Food is symbolic of love when words are inadequate.”
-            </span>
-            <span className="notFoundQuoteAuthor">— Alan D. Wolfelt</span>
+            {loading ? (
+              <div className="generatingQuote">Generating new quote...</div>
+            ) : (
+              <div className="flex flex-col justify-end">
+                <span className="notFoundQuoteWord">"{quote}"</span>
+                <span className="notFoundQuoteAuthor">—{author}-</span>
+              </div>
+            )}
           </div>
           <span className="wordNotFound pt-10">
             Click the button below to generate a new quote
           </span>
         </div>
-        <div className="quotesButtonNFContainer pt-8">
-          <button className="quotesButtonNF">FAMILY</button>
-          <button className="quotesButtonNF">FOOD</button>
-          <button className="quotesButtonNF">HAPPINESS</button>
-          <button className="quotesButtonNF">DESIGN</button>
-          <button className="quotesButtonNF">EXPERIENCE</button>
-          <button className="quotesButtonNF">HEALTH</button>
+        <div className="quotesButtonNFContainer pt-8 items-end">
+          <button
+            className="quotesButtonNF"
+            onClick={() => handleCategoryChange("family")}
+          >
+            FAMILY
+          </button>
+          <button
+            className="quotesButtonNF"
+            onClick={() => handleCategoryChange("food")}
+          >
+            FOOD
+          </button>
+          <button
+            className="quotesButtonNF"
+            onClick={() => handleCategoryChange("happiness")}
+          >
+            HAPPINESS
+          </button>
+          <button
+            className="quotesButtonNF"
+            onClick={() => handleCategoryChange("design")}
+          >
+            DESIGN
+          </button>
+          <button
+            className="quotesButtonNF"
+            onClick={() => handleCategoryChange("experience")}
+          >
+            EXPERIENCE
+          </button>
+          <button
+            className="quotesButtonNF"
+            onClick={() => handleCategoryChange("health")}
+          >
+            HEALTH
+          </button>
         </div>
       </div>
       <div className="footer-comingsoon">
